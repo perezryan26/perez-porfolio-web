f79e5f816313aa129ab0c18eec491aee848ec0d0
Ryan Perez <perezryan26@gmail.com>
2023-10-04 21:15:34 -0700

    Slime Project 10/4/2023

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import Calculator from '../assets/images/calculator-project.png';
 import Odyssey from '../assets/images/employee-odyssey-project.png';
+import Slime from '../assets/images/slime-project4.png';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import { faCircleArrowRight } from '@fortawesome/free-solid-svg-icons';
@@ -13,7 +14,7 @@ function Banner() {
         <div className="projects-section debug">
             <h1 className="section-h1 text-start project-title-section causten-bold text-green debug">Personal Projects</h1>
             <div className="debug">
-                <div className="project-cont">
+                <div className="project-cont margin-btm-project">
                     <div className="project-img-cont">
                         <img className="project-img" src={Odyssey} alt="Employee Odyssey"/>
                         <div className="project-img-after"></div>
@@ -52,40 +53,80 @@ function Banner() {
                     </div>
                 </div>
 
-                <div className="project-cont-reverse">
+                <div className="project-cont-reverse margin-btm-project">
                 <div className="text-white">
                         <h1 className="project-title causten-bold cursor-default">
                             <div className="text-cont-reverse">
                                 <div className="hidden-cont">
-                                    Calculator
+                                    Slime Emporium
                                 </div>
                             </div>
                         </h1>
-                        <p className="project-para-reverse causten-light cursor-default text-offwhite">An intuitive calculator app, taking inspiration from the iPhone's calculator application.
+                        <p className="project-para-reverse causten-light cursor-default text-offwhite">A modern e-commerce store with a whimsical slime theme for a younger audience.
                         </p>
                         <div>
                             <div className="project-badge-row-reverse">
-                                <span className="badge background-green causten-bold language-badge">REACT</span>
-                                <span className="badge background-green causten-bold language-badge ms-2">HTML</span>
-                                <span className="badge background-green causten-bold language-badge ms-2">CSS</span>
+                                <span className="badge background-green causten-bold language-badge">C#</span>
+                                <span className="badge background-green causten-bold language-badge ms-2">ASP.NET</span>
+                                <span className="badge background-green causten-bold language-badge ms-2">SQL</span>
+                                <span className="badge background-green causten-bold language-badge ms-2">Bootstrap</span>
                             </div>
                         </div>
                         <div className="project-button-row-reverse">
                             <div>
-                                <a class="icon2" href="https://github.com/perezryan26/reactcalculatorwebapp">
+                                <a class="icon2" href="https://github.com/perezryan26/slimestorewebapp">
                                     <FontAwesomeIcon icon={faGithub} class="project-icon text-offwhite" />
                                 </a>
                             </div>
                             <div className="ps-4">
-                                <a href="https://ryan-perez-calculator.netlify.app/"> 
+                                <a href="https://ryanperezportfolio.netlify.app/">
                                     <FontAwesomeIcon icon={faCircleArrowRight} class="project-icon text-offwhite" />
                                 </a>
                             </div>
                         </div>
                     </div>
                     <div className="project-img-cont">
-                        <img className="project-img" src={Calculator} alt="Employee Odyssey"/>
+                        <img className="project-img" src={Slime} alt="Slime Emporium"/>
                         <div className="project-img-after"></div>
+                    </div>
+                </div>
+
+
+                <div className="project-cont">
+                <div className="project-img-cont">
+                        <img className="project-img" src={Calculator} alt="Calculator"/>
+                        <div className="project-img-after"></div>
+                    </div>
+                    <div className="text-white">
+                        <h1 className="project-title causten-bold cursor-default">
+                            <div className="text-cont">
+                                <div className="hidden-cont">
+                                Calculator
+                                </div>
+                            </div>
+                        </h1>
+                        <p className="project-para causten-light cursor-default text-offwhite">
+                            An intuitive calculator app, taking inspiration from the iPhone's calculator application.
+                        </p>
+                        <div>
+                            <div className="project-badge-row">
+                                <span className="badge background-green causten-bold language-badge">REACT</span>
+                                <span className="badge background-green causten-bold language-badge ms-2">HTML</span>
+                                <span className="badge background-green causten-bold language-badge ms-2">CSS</span>
+                            </div>
+                        </div>
+                        <div className="project-button-row">
+                            <div>
+                                <a class="icon2" href="https://github.com/perezryan26/reactcalculatorwebapp">
+                                    <FontAwesomeIcon icon={faGithub} class="project-icon text-offwhite" />
+                                </a>
+                            </div>
+                            <div className="ps-4">
+                                <a href="https://ryan-perez-calculator.netlify.app/">
+                                    <FontAwesomeIcon icon={faCircleArrowRight} class="project-icon text-offwhite" />
+                                </a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
